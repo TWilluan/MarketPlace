b931bc0127125dee987d23dfba5d74f409d2b1f4
TWilluan <tuanvo.will@gmail.com>
2024-07-09 05:22:17 +0700

    add navbar

---
+++ marketplace/src/config/index.ts
@@ -0,0 +1,45 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor pick",
+        href: "#",
+        imageSrc: "/nav/ui_kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/ui_kits/blue.jpg",
+      },
+      {
+        name: "Best Sellers",
+        href: "#",
+        imageSrc: "/nav/ui_kits/purple.jpg",
+      },
+    ],
+  },
+
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Editor pick",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Best Sellers",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
