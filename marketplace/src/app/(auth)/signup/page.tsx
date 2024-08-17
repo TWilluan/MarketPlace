6b2d906ef604b7e5db2a3e63eb35ea32f671bf69
TWilluan <tuanvo.will@gmail.com>
2024-08-17 23:08:48 +0700

    implement frontend signup page

---
+++ marketplace/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { Icons } from "@/components/Icons"
+import { buttonVariants } from "@/components/ui/button"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
+
+const Page = () => {
+    return (
+        <>
+            <div className="container relative flex p-20 flex-col  items-center justify-center lg:px-0">
+                <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                    <div className="flex flex-col items-center space-y-2 text-center">
+                        <Icons.logo className="size-20"/>
+                        <h1 className="text-2xl font-bold">
+                            Create an Account
+                        </h1>
+                        <Link href="/signin" className={buttonVariants({
+                            variant: 'link',
+                            className: 'gap-1.5'
+                        })}>
+                            Already have an account? Sign in
+                            <ArrowRight className="h-4 w-4"/>
+                        </Link>
+                    </div>
+
+                    <div className="grid gap-6">
+                        <form onSubmit={}>
+                            <div className="grid gap-2">
+                                <div className="grid gap-1 py-2">
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Page
